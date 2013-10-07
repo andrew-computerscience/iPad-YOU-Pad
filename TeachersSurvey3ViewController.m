48fc2f7699f0cc97e4714dff51a30206070dc3d9
joshlava <joshlaverghetta@192-168-1-12.tpgi.com.au>
2013-10-07 13:50:22 +0800

    moar

---
+++ TeachersSurvey3ViewController.m
@@ -0,0 +1,38 @@
+//
+//  TeachersSurvey3ViewController.m
+//  iPad-YOU-Pad
+//
+//  Created by Josh La Verghetta on 7/10/13.
+//  Copyright (c) 2013 Andrew Edwards. All rights reserved.
+//
+
+#import "TeachersSurvey3ViewController.h"
+
+@interface TeachersSurvey3ViewController ()
+
+@end
+
+@implementation TeachersSurvey3ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
