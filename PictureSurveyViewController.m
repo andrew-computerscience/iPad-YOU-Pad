fd919ade4b25bcfca0bebec8724bd57bbf601509
pangpang1987 <pangpang.i.lily@gmail.com>
2013-10-17 14:43:11 +0800

    Fix the keyboard on pic survey

--- PictureSurveyViewController.m
+++ PictureSurveyViewController.m
@@ -399,4 +399,37 @@ NSFileManager *fmPS;
     // Dispose of any resources that can be recreated.
 }
 
+/*
+### Fix the keyboard problem
+ */
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self animateTextField: textField up: YES];
+}
+
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self animateTextField: textField up: NO];
+}
+
+- (void) animateTextField: (UITextField*) textField up: (BOOL) up
+{
+    const int movementDistance = 180; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+    
+    int movement = (up ? -movementDistance : movementDistance);
+    
+    [UIView beginAnimations: @"anim" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
+}
+/*
+ * Fix the keyboard problem
+ 
+ #  End
+ */
+
 @end
